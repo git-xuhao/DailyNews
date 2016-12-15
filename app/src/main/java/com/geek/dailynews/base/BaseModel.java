5884364a00eae920b1077a2c0df960634a60de99
xuhao <504105930@qq.com>
2016-12-15 19:15:31 +0800

    init

---
+++ app/src/main/java/com/geek/dailynews/base/BaseModel.java
@@ -0,0 +1,11 @@
+package com.geek.dailynews.base;
+
+/**
+ * MyBlog: xuhaoblog.com
+ * GitHub: github.com/git-xuhao
+ * Created by Xiho
+ * on 2016.12.14  17:05
+ * Function:
+ */
+public class BaseModel {
+}
---
+++ app/src/main/java/com/geek/dailynews/base/IView.java
@@ -0,0 +1,11 @@
+package com.geek.dailynews.base;
+
+/**
+ * MyBlog: xuhaoblog.com
+ * GitHub: github.com/git-xuhao
+ * Created by Xiho
+ * on 2016.12.13  18:05
+ * Function:
+ */
+public interface IView {
+}
